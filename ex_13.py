bb04b1b0dacd319a38291eeda6c98285abcac0e3
ekatellv <159680448+ekatellv@users.noreply.github.com>
2024-06-21 12:22:14 +0700

    Update ex_13.py

--- ex_13.py
+++ ex_13.py
@@ -3,4 +3,5 @@ import random
 line = int(input('Введите количество строк: '))
 column = int(input('Введите количество столбцов: '))
 page = int(input('Введите количество страниц: '))
-print(f'страница {random.randint(1, page)}, столбец {random.randint(1, column)}, строка {random.randint(1, line)}')
+print(f'страница {random.randint(1, page)}, столбец {random.randint(1, column)}, '
+      f'строка {random.randint(1, line)}')
